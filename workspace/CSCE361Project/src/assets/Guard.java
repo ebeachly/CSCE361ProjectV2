bbdbf9c551aa1bee65f8118497e0d7229cab8515
David Shriver <dlshriver@cox.net>
2014-03-18 14:53:48 -0500

    Winning
    win scenario added to game, game loop updated

--- workspace/CSCE361Project/src/assets/Guard.java
+++ workspace/CSCE361Project/src/assets/Guard.java
@@ -35,16 +35,30 @@ public class Guard extends Aspect {
 					System.out.println("The door swings open.");
 					System.out.println("You have been caught.");
 					Game.hasLost = true;
+				} else if (Game.player.currentLocation == World.prisonHallway) {
+					System.out
+							.println("The guard is confused by your absence.");
+					Game.hasWon = true;
 				}
+			} else if (position == 5 || position == -1) {
+				System.out.println("The guard turns around");
+				delta *= -1;
 			} else if (Math.abs(position + delta - 2) > Math.abs(position - 2)) {
-				System.out.println("The guard walks down the hall");
+				System.out.println("The guard walks away from your cell");
+				if (Game.player.currentLocation == World.prisonHallway) {
+					System.out.println("You escape silently into the shadows.");
+					Game.hasWon = true;
+				}
 			} else {
 				System.out.println("The guard walks toward your cell");
+				if (Game.player.currentLocation == World.prisonHallway) {
+					System.out
+							.println("You step out of your cell in full view of the guard.");
+					System.out.println("You have been caught.");
+					Game.hasWon = true;
+				}
 			}
 			position += delta;
-			if (position == 4 || position == 0) {
-				delta *= -1;
-			}
 		} else {
 			return;
 		}
--- workspace/CSCE361Project/src/superClasses/Game.java
+++ workspace/CSCE361Project/src/superClasses/Game.java
@@ -9,55 +9,61 @@ public class Game {
 	public static Player player = new Player();
 	public static Parser parser = new Parser();
 	public static boolean hasLost = false;
+	public static boolean hasWon = false;
 	public static Queue<Aspect> aspects;
 
 	public static void main(String args[]) {
 		// TODO: Implement
+		@SuppressWarnings("resource")
 		Scanner console = new Scanner(System.in);
 		String input;
-		System.out
-				.println(""
-						+ "     ____.     ___.  ___.                                      __           \n"
-						+ "    |    |____ \\_ |__\\_ |__   _____________  _  ______   ____ |  | _____.__.\n"
-						+ "    |    \\__  \\ | __ \\| __ \\_/ __ \\_  __ \\ \\/ \\/ /  _ \\_/ ___\\|  |/ <   |  |\n"
-						+ "/\\__|    |/ __ \\| \\_\\ \\ \\_\\ \\  ___/|  | \\/\\     (  <_> )  \\___|    < \\___  |\n"
-						+ "\\________(____  /___  /___  /\\___  >__|    \\/\\_/ \\____/ \\___  >__|_ \\/ ____|\n"
-						+ "              \\/    \\/    \\/     \\/                         \\/     \\/\\/     ");
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		do {
-			System.out.print(">> ");
-			input = console.nextLine();
-		} while (Parser.parseMenuOption(input) == 0);
-
-		// Do the second phase of world construction (First was done when the
-		// program was loaded)
-		World.constructWorld();
-		// clear aspect queue
-		clearAspectQueue();
-		// Put the player in the cell
-		Game.player.currentLocation = World.prisonCell;
-
-		player.currentLocation.printDescription();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-
-		while (!hasLost) {
+		while (true) {
+			System.out
+					.println(""
+							+ "     ____.     ___.  ___.                                      __           \n"
+							+ "    |    |____ \\_ |__\\_ |__   _____________  _  ______   ____ |  | _____.__.\n"
+							+ "    |    \\__  \\ | __ \\| __ \\_/ __ \\_  __ \\ \\/ \\/ /  _ \\_/ ___\\|  |/ <   |  |\n"
+							+ "/\\__|    |/ __ \\| \\_\\ \\ \\_\\ \\  ___/|  | \\/\\     (  <_> )  \\___|    < \\___  |\n"
+							+ "\\________(____  /___  /___  /\\___  >__|    \\/\\_/ \\____/ \\___  >__|_ \\/ ____|\n"
+							+ "              \\/    \\/    \\/     \\/                         \\/     \\/\\/     ");
+			System.out.println();
+			System.out.println();
+			System.out.println();
 			do {
 				System.out.print(">> ");
 				input = console.nextLine();
-			} while (!parser.parseInputAndExecute(input));
-			aspects.addAll(World.globalAspects);
-			aspects.addAll(player.currentLocation.aspects);
-			while (!aspects.isEmpty()) {
-				aspects.poll().takeTurn();
-			}
-		}
+			} while (Parser.parseMenuOption(input) == 0);
 
-		System.out.println("YOU LOSE!");
-		console.close();
+			// Do the second phase of world construction (First was done when
+			// the
+			// program was loaded)
+			World.constructWorld();
+			// clear aspect queue
+			clearAspectQueue();
+			// Put the player in the cell
+			Game.player.currentLocation = World.prisonCell;
+
+			player.currentLocation.printDescription();
+
+			while (!hasLost && !hasWon) {
+				do {
+					System.out.print(">> ");
+					input = console.nextLine();
+				} while (!parser.parseInputAndExecute(input));
+				aspects.addAll(World.globalAspects);
+				aspects.addAll(player.currentLocation.aspects);
+				while (!aspects.isEmpty()) {
+					aspects.poll().takeTurn();
+				}
+			}
+
+			if (hasWon) {
+				System.out.println("YOU WON!");
+			} else {
+				System.out.println("YOU LOSE!");
+			}
+
+		}
 
 		// Main Menu
 
