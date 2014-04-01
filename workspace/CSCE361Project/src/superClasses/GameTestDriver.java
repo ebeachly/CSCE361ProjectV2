c7d4ee3635038edb145be8c30ffe7b22dc1f138a
David Shriver <dlshriver@cox.net>
2014-04-01 15:16:00 -0500

    Tester

--- workspace/CSCE361Project/src/superClasses/GameTestDriver.java
+++ workspace/CSCE361Project/src/superClasses/GameTestDriver.java
@@ -1,9 +1,34 @@
 package superClasses;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
 public class GameTestDriver {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		InputStream in = new ByteArrayInputStream(("new\n" +
+				"pick up key\n" +
+				"wait\n" +
+				"unlock door with key\n" +
+				"go through door\n" +
+				"quit\n" +
+				"yes").getBytes());
+		System.setIn(in);
+		PrintStream out = new PrintStream("testing.txt");
+		PrintStream stdout = System.out;
+		System.setOut(out);
+		try{
+			Game.main(null);
+		} catch (NoSuchElementException nsee) {
+			
+		}
+		System.setOut(stdout);
+		System.out.println();
 	}
 
 }
