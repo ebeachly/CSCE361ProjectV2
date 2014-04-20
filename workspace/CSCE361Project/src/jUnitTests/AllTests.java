9c0d8e434cb20e45bd740d5bd8c32fd0190438ce
ebeachly <ebeachly@gmail.com>
2014-04-20 13:01:56 -0500

    Created a Test Suite

---
+++ workspace/CSCE361Project/src/jUnitTests/AllTests.java
@@ -0,0 +1,12 @@
+package jUnitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAspect.class, TestInteractable.class, TestItem.class,
+		TestLocation.class })
+public class AllTests {
+
+}
