528366528fe200ea08617717356cdbc2c06f00ad
ryanclaybrooks <ryanclaybrooks@yahoo.com>
2014-04-24 15:58:57 -0500

    Updated merchant
    Merchant now kills player

--- workspace/CSCE361Project/src/assets/Merchant.java
+++ workspace/CSCE361Project/src/assets/Merchant.java
@@ -3,6 +3,7 @@ package assets;
 import superClasses.Aspect;
 import superClasses.Game;
 import superClasses.Interactable;
+import superClasses.Item;
 
 public class Merchant extends Aspect {
 
@@ -23,6 +24,12 @@ public class Merchant extends Aspect {
 		System.out.println("A rotund man with a beard, and "
 				+ "wearing a green cloak. He is standing next to his wagon and pointing at a sign"+
 				"that reads: \" Shield for sale\".");
+		return true;
+	}
+	public boolean damage(Item weapon){
+		System.out.println("You tried to attack the merchant.\n"+
+		"He responds with a storm of lightning bolts killing you.");
+		Game.hasLost = true;
 		return true;
 	}
 
